a9bbbc905dcabc220761e6ed51c7f7effda60c88
Eytyy <e.tayyem@gmail.com>
2019-10-27 13:18:29 +0200

    installed react-axe and code cleanup

--- src/App.js
+++ src/App.js
@@ -1,37 +1,13 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { hot } from 'react-hot-loader'
 import styled from 'styled-components'
 
-const Warning = React.lazy(() => import('./Warning'))
-const Count = styled.h2`
+const Title = styled.h1`
   font-size: 2em;
-  &.warning {
-    color: red;
-  }
 `
 
 const App = () => {
-  const [count, updateCount] = useState(0)
-
-  function increment() {
-    updateCount(c => c + 1)
-  }
-
-  function decrement() {
-    updateCount(c => c - 1)
-  }
-  return (
-    <>
-      <Count className={count > 10 ? 'warning' : null}> {count} </Count>{' '}
-      <button onClick={increment}> + </button>{' '}
-      <button onClick={decrement}> - </button>{' '}
-      {count > 10 ? (
-        <React.Suspense fallback={null}>
-          <Warning />
-        </React.Suspense>
-      ) : null}{' '}
-    </>
-  )
+  return <Title>hi</Title>
 }
 
 export default hot(module)(App)
--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import DefaultErrorBoundry from './DefaultErrorBoundry'
 
+if (process.env.Node_ENV === 'development') {
+  const axe = require('react-axe')
+  axe(React, ReactDOM, 1000)
+}
+
 ReactDOM.render(
   <React.StrictMode>
     <DefaultErrorBoundry>
